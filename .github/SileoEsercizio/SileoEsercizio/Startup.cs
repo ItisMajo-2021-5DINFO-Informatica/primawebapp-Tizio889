ea11f64f267542bd9df5ca4b23b2ea3ff3f06164
Tizio889 <98579391+Tizio889@users.noreply.github.com>
2022-02-07 11:25:59 +0100

    Razor

--- .github/SileoEsercizio/SileoEsercizio/Startup.cs
+++ .github/SileoEsercizio/SileoEsercizio/Startup.cs
@@ -12,10 +12,15 @@ namespace SileoEsercizio
 {
     public class Startup
     {
+
+
+
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +39,8 @@ namespace SileoEsercizio
                 {
                     await context.Response.WriteAsync("Cerea");
                 });
+
+                endpoints.MapRazorPages();
             });
         }
     }
