b3990de46cd45d9aef5ef30e59caa12148a601e5
Tizio889 <98579391+Tizio889@users.noreply.github.com>
2022-02-07 12:07:56 +0100

    AggiuntaPag1

--- .github/SileoEsercizio/SileoEsercizio/Startup.cs
+++ .github/SileoEsercizio/SileoEsercizio/Startup.cs
@@ -12,10 +12,6 @@ namespace SileoEsercizio
 {
     public class Startup
     {
-
-
-
-        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -35,12 +31,8 @@ namespace SileoEsercizio
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Cerea");
-                });
-
                 endpoints.MapRazorPages();
+               
             });
         }
     }
